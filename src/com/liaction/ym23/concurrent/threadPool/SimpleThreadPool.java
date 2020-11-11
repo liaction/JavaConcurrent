54a570998b8401e52b5ca1f12591be89e836e2ac
liaction <ch.en.si@qq.com>
2020-11-11 13:35:16 +0800

    Initial commit

---
+++ src/com/liaction/ym23/concurrent/threadPool/SimpleThreadPool.java
@@ -0,0 +1,162 @@
+package com.liaction.ym23.concurrent.threadPool;
+
+import java.util.LinkedList;
+
+public class SimpleThreadPool extends Thread {
+
+    private final static int DEFAULT_POOL_SIZE = 10;
+    private final int poolSize;
+    private int thread_seq = 0;
+    private final static String THREAD_PREFIX = "Thread-ym23-";
+    private final static String THREAD_GROUP_NAME = "Thread_pool_group";
+
+    private final static LinkedList<Runnable> WORK_QUEUE = new LinkedList<>();
+    private final static LinkedList<WorkTask> THREAD_QUEUE = new LinkedList<>();
+
+    private final static Object THREAD_POOL_LOCK = new Object();
+    private final static Object LOCK = new Object();
+
+    private boolean destroyed = false;
+
+    public SimpleThreadPool() {
+        this(DEFAULT_POOL_SIZE);
+    }
+
+    public SimpleThreadPool(int poolSize) {
+        this.poolSize = poolSize;
+        initThreadPool();
+    }
+
+    private void initThreadPool() {
+        start();
+    }
+
+    public void addTask(Runnable runnable) {
+        if (isDestroyed()) {
+            throw new IllegalStateException("thread pool is died, so you should not add task again.");
+        }
+        synchronized (WORK_QUEUE) {
+            WORK_QUEUE.addLast(runnable);
+            WORK_QUEUE.notifyAll();
+        }
+    }
+
+    public void shutDown(long waitTime) throws InterruptedException {
+        shutDown(waitTime > 0, waitTime);
+    }
+
+    public void shutDown(boolean now, long waitTime) throws InterruptedException {
+        if (!now) {
+            shutDown();
+            return;
+        }
+        synchronized (THREAD_POOL_LOCK) {
+            THREAD_POOL_LOCK.wait(waitTime);
+            interrupt();
+            destroyed = true;
+            System.out.println("the thread pool server is destroyed now.");
+        }
+    }
+
+    public void shutDown() throws InterruptedException {
+        // 等待任务做完
+        while (!WORK_QUEUE.isEmpty()) {
+            Thread.sleep(23);
+        }
+        int currentThreadSize = THREAD_QUEUE.size();
+        while (currentThreadSize > 0) {
+            for (WorkTask workTask :
+                    THREAD_QUEUE) {
+                if (workTask.getWorkState() == WorkState.BLOCKED) {
+                    workTask.interrupt();
+                    workTask.close();
+                    currentThreadSize--;
+                }
+            }
+            Thread.sleep(23);
+        }
+        destroyed = true;
+        System.out.println("thread pool is destroyed.");
+    }
+
+    public int getPoolSize() {
+        return poolSize;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    private enum WorkState {
+        IDLE, RUNNING, BLOCKED, DEAD
+    }
+
+    @Override
+    public void run() {
+        // 添加
+        for (int i = 0; i < poolSize; i++) {
+            WorkTask workTask = createWorkTask();
+            THREAD_QUEUE.addLast(workTask);
+            workTask.setDaemon(true);
+            workTask.start();
+        }
+        System.out.println(THREAD_QUEUE.size());
+        synchronized (LOCK) {
+            try {
+                LOCK.wait();
+            } catch (InterruptedException e) {
+                System.out.println("the thread pool is destroyed now.");
+            }
+        }
+    }
+
+    private WorkTask createWorkTask() {
+        return new WorkTask(new ThreadGroup(THREAD_GROUP_NAME), THREAD_PREFIX + (thread_seq++));
+    }
+
+    private static class WorkTask extends Thread {
+
+        private volatile WorkState workState = WorkState.IDLE;
+
+        WorkTask(ThreadGroup threadGroup, String name) {
+            super(threadGroup, name);
+        }
+
+        @Override
+        public void run() {
+            OUTER:
+            while (!isDead()) {
+                Runnable runnable;
+                synchronized (WORK_QUEUE) {
+                    while (WORK_QUEUE.isEmpty()) {
+                        try {
+                            this.workState = WorkState.BLOCKED;
+                            WORK_QUEUE.wait();
+                        } catch (InterruptedException e) {
+                            break OUTER;
+                        }
+                    }
+                    this.workState = WorkState.RUNNING;
+                    runnable = WORK_QUEUE.removeFirst();
+                }
+                if (runnable != null) {
+                    runnable.run();
+                }
+                this.workState = WorkState.IDLE;
+            }
+        }
+
+        public WorkState getWorkState() {
+            return workState;
+        }
+
+        public boolean isDead() {
+            return getWorkState() == WorkState.DEAD;
+        }
+
+        public void close() {
+            workState = WorkState.DEAD;
+        }
+    }
+
+}
---
+++ src/com/liaction/ym23/concurrent/threadPool/SimpleThreadPoolTest.java
@@ -0,0 +1,44 @@
+package com.liaction.ym23.concurrent.threadPool;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class SimpleThreadPoolTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleThreadPool simpleThreadPool = new SimpleThreadPool();
+        IntStream.rangeClosed(0,40).forEach(index->{
+            simpleThreadPool.addTask(()->{
+                Optional.of("The task " + index + " start." + " service thread: " + Thread.currentThread()).ifPresent(System.out::println);
+                try {
+                    if (index == 23){
+                        while (true){}
+                    }else {
+                        Thread.sleep(1_000L);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Optional.of("The task " + index + " end." + " service thread: " + Thread.currentThread()).ifPresent(System.out::println);
+            });
+        });
+        try {
+            Thread.sleep(1_000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        simpleThreadPool.shutDown(5_000L);
+        IntStream.rangeClosed(0,23).forEach(index->{
+            simpleThreadPool.addTask(()->{
+                Optional.of("The task " + index + " start." + " service thread: " + Thread.currentThread()).ifPresent(System.out::println);
+                try {
+                    Thread.sleep(1_000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Optional.of("The task " + index + " end." + " service thread: " + Thread.currentThread()).ifPresent(System.out::println);
+            });
+        });
+    }
+
+}
